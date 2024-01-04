4e1e50925638ab182605d34d93dade69d3ee11da
AlexMandrik005 <152302697+AlexMandrik005@users.noreply.github.com>
2024-01-04 21:21:23 +0300

    Add files via upload

---
+++ Laba6Narkevich/case2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+#include <Windows.h>
+
+int case2()
+{
+	using namespace std;
+	SetConsoleCP(1251);
+	char a, b;
+	int h, i;
+	cout << "Введите количество повторений" << endl;
+	cin >> h;
+	for (i = 0; i < h; i++)
+	{
+		cout << "Введите символ нижнего регистра" << endl;
+		cin >> a;
+		cout << "Введите символ верхнего регистра" << endl;
+		cin >> b;
+		if ((b >= 'А') && (b <= 'Я') || (a >= 'а') && (a <= 'я')) {
+			cout << dec << abs(int(b) - int(a)) << endl << endl;
+		}
+		else {
+			cout << "Ошибка" << endl;
+		}
+	}
+	return 0;
+}
